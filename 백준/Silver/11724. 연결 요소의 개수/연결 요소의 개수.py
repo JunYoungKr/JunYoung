3b89f8496016769304955fe69616fbdefeaa23ea
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2024-01-31 23:49:04 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 680 ms, Memory: 65084 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,35 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+# 정점의 개수 N, 간선의 개수 M
+N, M = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
+
+# 그래프 작성
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+# print(graph)
+visited = [False] * (N + 1)
+
+# dfs 기본 코드
+def dfs(graph, v, visited):
+    visited[v] = True
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+# 연결된 노드의 수
+count = 0
+
+# dfs가 끝날 때마다
+for i in range(1, N + 1):
+    if not visited[i]:
+        dfs(graph, i, visited)
+        count += 1
+print(count)
