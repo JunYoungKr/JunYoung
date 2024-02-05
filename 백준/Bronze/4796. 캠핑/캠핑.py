6ee19f3269307c3c77a1a73c8b11db99d959b55c
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2024-02-05 14:07:27 +0900

    [Bronze I] Title: 캠핑, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4796. 캠핑/캠핑.py
+++ 백준/Bronze/4796. 캠핑/캠핑.py
@@ -1,11 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+
 i = 0
 while True:
-    i+=1
-    l, p, v = map(int, input().split())
-    if l==0 and p==0 and v==0:
+    i += 1
+    L, P, V = map(int, input().split())
+    if L == P == V == 0:
         break
-    a = v//p
-    b = v%p
-    if l<b:
-        b = l
-    print("Case %d: %d" %(i, a*l+b))
+    # L, P, V가
+    # 5, 8, 20일때
+    # 8일 중 5일 동안만 사용가능, V일 짜리 휴가
+    a = V // P
+    b = V % P
+    if L < b:
+        b = L
+    print("Case %d: %d" % (i, a * L + b))
