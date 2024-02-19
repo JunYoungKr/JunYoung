31dc2ccc9ec07eb3667ebae9813cbc261d90de9a
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2024-02-19 16:37:08 +0900

    [Silver I] Title: 동물원, Time: 88 ms, Memory: 35632 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0] * 100001
+
+dp[1] = 3
+dp[2] = 7
+
+for i in range(3, N+1):
+    dp[i] = (dp[i-1] * 2 + dp[i-2]) % 9901
+
+print(dp[N])
