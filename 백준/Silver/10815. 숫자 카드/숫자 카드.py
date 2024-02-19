96fdbf2101f212f33ef58d2f1d61497d42d16fa8
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2024-02-19 22:53:48 +0900

    [Silver V] Title: 숫자 카드, Time: 692 ms, Memory: 125176 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,17 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-arr = list(map(int, input().split()))
+
+arr = set(map(int, input().split()))
+
 M = int(input())
+
 arr2 = list(map(int, input().split()))
 
-dic = {}
-
 for i in arr2:
-    dic[i] = 0
-
-
-for i in arr:
-    if i in dic:
-        dic[i] = 1
-
-for i in dic:
-    print(dic[i], end=" ")
+    if i in arr:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
