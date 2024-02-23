5780334303fb472d38ce06f9f0fcd219e3e4357e
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2024-02-23 21:37:58 +0900

    [Silver III] Title: 이친수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -4,13 +4,10 @@ input = sys.stdin.readline
 N = int(input())
 
 dp = [0] * (N+1)
-# print(dp)
 
 dp[0] = 1
-dp[1] = 1
 
-for i in range(2, N+1):
-    dp[i] = dp[i-2] + dp[i-1]
-    # print(dp)
+for i in range(1, N+1):
+    dp[i] = dp[i-1] + dp[i-2]
 
 print(dp[N-1])
