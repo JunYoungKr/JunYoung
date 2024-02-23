51b349d4f5f78278459cf68e67ca63574125cf50
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2024-02-23 16:55:24 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 35228 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,29 +1,26 @@
-import sys
 from collections import deque
+import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-# 수빈이는 점 N, 동생은 점 K
-# 수빈이의 현재 위치가 X일때
-# 걷는다면 1초후에는 X-1 or X+1
-# 순간이동은 2*X
 
 N, K = map(int, input().split())
 
 visited = [0 for _ in range(100001)]
 
 
-def bfs(start):
+def bfs(start, end):
     queue = deque([start])
+
     while queue:
         v = queue.popleft()
-        # 수빈이와 동생을 찾은 경우
         if v == K:
             return visited[v]
-        # 이동을 하는 경우
-        for i in (v-1, v+1, v*2):
+        for i in (v+1, v-1, v*2):
+            # print(i)
             if 0 <= i <= 100000 and not visited[i]:
                 visited[i] = visited[v] + 1
                 queue.append(i)
 
 
-print(bfs(N))
+print(bfs(N, K))
