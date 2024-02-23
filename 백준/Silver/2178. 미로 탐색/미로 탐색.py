8c9aa820dc7ebd4a6c7f126f57c70c8c3e86e625
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2024-02-23 15:50:16 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,14 +1,12 @@
-import sys
 from collections import deque
+import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 N, M = map(int, input().split())
 
-graph = [list(map(int, input().strip())) for _ in range(N)]
-
-# print()
-# print(graph)
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().strip())))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -20,17 +18,19 @@ def bfs(x, y):
 
     while queue:
         x, y = queue.popleft()
-
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+
             if nx < 0 or ny < 0 or nx >= N or ny >= M:
                 continue
             if graph[nx][ny] == 0:
                 continue
+
             if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+
     return graph[N-1][M-1]
 
 
