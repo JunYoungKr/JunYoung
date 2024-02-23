62f7d5a5934f3c833df2a41daf3ff9ac76642c2b
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2024-02-23 16:00:16 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,40 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+# 컴퓨터의 수
+N = int(input())
+
+# 쌍의 수
+M = int(input())
+
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+# print(graph)
+
+visited = [False] * (N+1)
+
+# res = 0
+
+
+def bfs(v, graph):
+    visited[v] = True
+    queue = deque([v])
+    res = 0
+    while queue:
+        v = queue.popleft()
+        # res += 1
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+                res += 1
+    print(res)
+
+
+bfs(1, graph)
