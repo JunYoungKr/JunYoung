7849f37093b4bbfaf8b61a94db4249f0f67118bc
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2024-02-23 16:02:32 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -22,19 +22,34 @@ visited = [False] * (N+1)
 # res = 0
 
 
-def bfs(v, graph):
+# def bfs(v, graph):
+#     visited[v] = True
+#     queue = deque([v])
+#     res = 0
+#     while queue:
+#         v = queue.popleft()
+#         # res += 1
+#         for i in graph[v]:
+#             if not visited[i]:
+#                 visited[i] = True
+#                 queue.append(i)
+#                 res += 1
+#     print(res)
+
+
+# bfs(1, graph)
+
+res = 0
+
+
+def dfs(v, graph):
     visited[v] = True
-    queue = deque([v])
-    res = 0
-    while queue:
-        v = queue.popleft()
-        # res += 1
-        for i in graph[v]:
-            if not visited[i]:
-                visited[i] = True
-                queue.append(i)
-                res += 1
-    print(res)
+    global res
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i, graph)
+            res += 1
+    return res
 
 
-bfs(1, graph)
+print(dfs(1, graph))
