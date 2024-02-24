b3bd0bb71fc750b20cf3abb22a9f485bbfa64066
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2024-02-24 12:28:23 +0900

    [level 3] Title: 네트워크, Time: 0.52 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,30 +1,24 @@
 def dfs(graph, v, visited):
-    # 현재 노드를 방문 처리
     visited[v] = True
-    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
     for i in graph[v]:
         if not visited[i]:
             dfs(graph, i, visited)
-
-
+            
 def solution(n, computers):
     answer = 0
-
-    graph = [[] for _ in range(n)]
+    
+    graph = [[] for _ in range(len(computers))]
     visited = [False] * n
-    for i in range(n):
-        for j in range(n):
-            if computers[i][j] == 1 and i != j:
+    for i in range(len(computers)):
+        for j in range(len(computers)):
+            if i != j and computers[i][j] == 1:
                 graph[i].append(j)
-
-    for i in range(n):
+    
+    for i in range(len(computers)):
         if not visited[i]:
             dfs(graph, i, visited)
-            answer += 1
-    # print(graph)
+            answer +=1
+        
+    
+
     return answer
-
-
-n = 3
-computers = [[1, 1, 0], [1, 1, 1], [0, 1, 1]]
-print(solution(n, computers))
