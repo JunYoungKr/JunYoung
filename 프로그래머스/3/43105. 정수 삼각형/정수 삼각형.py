ddc6f30b529296eac783c54aac12a132c71bf72b
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2024-02-24 11:37:54 +0900

    [level 3] Title: 정수 삼각형, Time: 55.55 ms, Memory: 14.3 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,26 +1,19 @@
 def solution(triangle):
     answer = 0
     
-    # 첫 자리 더해주기
-    triangle [0][0] = triangle[0][0]
-    for i in range(1,len(triangle)):
-        triangle[i][0] += triangle[i-1][0]
-        
-    # 끝 자리 더해주기
+    triangle[0][0] = triangle[0][0]
     for i in range(1, len(triangle)):
-        for j in range(len(triangle[i])):
-            if i == j:
+        for j in range(len(triangle)):
+            if j == 0:
+                triangle[i][0] += triangle[i-1][0]
+            elif i == j:
                 triangle[i][j] += triangle[i-1][j-1]
-    # print(triangle)
-    
-    # 첫 과 끝 사이 최댓값으로 비교해서 더해주기
-    for i in range(1,len(triangle)):
-        for j in range(1,len(triangle[i])-1):
-            triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
-            
-    answer = 0
+    for i in range(1, len(triangle)):
+        for j in range(1, len(triangle[i])-1):
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+                
     for i in range(len(triangle)):
         for j in range(len(triangle[i])):
-            if answer < triangle[i][j]:
-                answer= triangle[i][j]
+            if triangle[i][j] > answer:
+                answer = triangle[i][j] 
     return answer
